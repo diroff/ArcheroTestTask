be961d35da6a76bb3e2fb9dd06fb529c409d2ddb
Диэр <danil.aprelskiy@mail.ru>
2024-08-29 18:39:31 +0300

    refactor: scripts path

---
+++ Assets/Scripts/Weapon/SplitShotWeapon.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitShotWeapon : RangedWeapon
+{
+    public SplitShotWeapon(WeaponData data, Fighter owner) : base(data, owner)
+    {
+    }
+}
